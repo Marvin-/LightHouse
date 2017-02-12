5838ba52ce5926c29a5eb6646c88b549d19a29ac
Marvin M <marvin23m@gmail.com>
2017-02-12 18:17:31 +0000

    Add edit ability

--- app.rb
+++ app.rb
@@ -22,6 +22,21 @@ class LightHouseApp < Sinatra::Base
     redirect '/'
   end
 
+  get '/:id/edit' do |id|
+    thought = Thought.find(id.to_i)
+    haml :edit, locals: {id: id, thought: thought}
+  end
+
+  put '/:id' do |id|
+    data = {
+      :title => params[:idea_title],
+      :descrption => params[:idea_description]
+    }
+
+    Thought.update(id.to_i, data)
+    redirect '/'
+  end
+
   delete '/:id' do |id|
     Thought.delete(id.to_i)
     redirect '/'
--- thought.rb
+++ thought.rb
@@ -32,7 +32,24 @@ class Thought
       database['thoughts'].delete_at(position)
     end
   end
+
+  def self.find(id)
+    raw_idea = find_raw_thought(id)
+    Thought.new(raw_idea[:title], raw_idea[:description])
+  end
+
+  def self.find_raw_thought(id)
+    database.transaction do 
+      database['thoughts'].at(id)
+    end
+  end
   
+  def self.update(id, data)
+    database.transaction do 
+      database["thoughts"][id] = data
+    end
+  end
+
   def self.database
     @database ||= YAML::Store.new("thoughts")
   end
